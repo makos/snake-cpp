842de1d9e3fd573de7af0582c00af6b03107ae92
Mateusz Makowski <matmakos@gmail.com>
2018-08-21 20:50:46 +0200

    Clean up

--- src/point.cpp
+++ src/point.cpp
@@ -1,6 +1,7 @@
 #include "point.hpp"
 
-Point::Point() {
+Point::Point()
+{
     y = 0;
     x = 0;
 }
@@ -9,14 +10,7 @@ Point::Point(int y, int x) : y(y), x(x) {}
 
 Point::Point(const Point &other) : y(other.y), x(other.x) {}
 
-bool Point::operator==(const Point &other) {
+bool Point::operator==(const Point &other)
+{
     return other.y == y && other.x == x;
 }
-
-// int Point::sety(int y) { this->y = y; }
-
-// int Point::setx(int x) { this->x = x; }
-
-// int Point::gety() { return y; }
-
-// int Point::getx() { return x; }
