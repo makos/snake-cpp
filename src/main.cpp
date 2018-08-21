ae542a1544cd87c89f0b5299d4c361d87fe89096
Mateusz Makowski <matmakos@gmail.com>
2018-08-21 20:50:33 +0200

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,31 @@
  * main.cpp - ncurses, game structures etc. initialization and hand-off.
  * This should be detached from the actual logic as much as possible.
  */
+#include "game.hpp"
 #include "player.hpp"
 #include "point.hpp"
 #include "render.hpp"
 #include <curses.h>
 
 int main(int argc, char *argv[]) {
-    Render init;
-    WINDOW *mainWindow = newwin(10, 20, 0, 0);
-    box(mainWindow, 0, 0);
+    Render render;
+    Game game;
 
-    wattron(mainWindow, COLOR_PAIR(Color::Red));
-    mvwprintw(mainWindow, 1, 1, "Dupa szatana");
-    wattroff(mainWindow, COLOR_PAIR(Color::Red));
+    box(game.getTopWindow(), 0, 0);
 
-    wrefresh(mainWindow);
-    wgetch(mainWindow);
+    wattron(game.getTopWindow(), COLOR_PAIR(Color::Red));
+    mvwprintw(game.getTopWindow(), 1, 1, "%p", game.getTopWindow());
+    wattroff(game.getTopWindow(), COLOR_PAIR(Color::Red));
+
+    game.refreshTopWindow();
+
+    while (game.isRunning()) {
+
+        game.getInput();
+        werase(game.getTopWindow());
+        mvwprintw(game.getTopWindow(), 1, 1, "%p", game.getTopWindow());
+        game.refreshTopWindow();
+    }
 
     return 0;
 }
