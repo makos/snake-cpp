3ce844c37a7236c4b0f98cc255b921d9da482110
Mateusz Makowski <matmakos@gmail.com>
2018-08-23 14:09:01 +0200

    Add Menu & MenuItem tests

---
+++ tests/test_Menu.cpp
@@ -0,0 +1,35 @@
+#include "Menu.hpp"
+#include "MenuItem.hpp"
+#include "catch.hpp"
+
+class Game {
+  public:
+    Game() : mIsRunning(true) {}
+    ~Game() {}
+
+    bool isRunning() { return mIsRunning; }
+    void setRunning(bool state) { mIsRunning = state; }
+
+  private:
+    bool mIsRunning;
+};
+
+void callback(Game &g) { g.setRunning(false); }
+
+TEST_CASE("Menu.hpp tests", "[menu]") {
+    Game mockGame;
+    Menu testMenu(mockGame);
+
+    SECTION("Testing addItem() and items()") {
+        testMenu.addItem("Test", callback);
+        REQUIRE(testMenu.items()[0]->text() == "Test");
+        REQUIRE(testMenu.items()[0]->id() == 0);
+    }
+
+    SECTION("Testing clickItem()") {
+        testMenu.addItem("Test", callback);
+        REQUIRE(mockGame.isRunning());
+        testMenu.clickItem(0);
+        REQUIRE(!mockGame.isRunning());
+    }
+}
