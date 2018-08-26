6fc8828ed5c6856bd5a5706ec777f75d284816c1
Mateusz Makowski <matmakos@gmail.com>
2018-08-26 13:17:46 +0200

    Move callbacks out to separate file

---
+++ include/State/Callback.hpp
@@ -0,0 +1,10 @@
+#pragma once
+class Game;
+
+namespace Callback {
+
+void continueClicked(Game &game);
+void newClicked(Game &game);
+void exitClicked(Game &game);
+
+} // namespace Callback
---
+++ src/State/Callback.cpp
@@ -0,0 +1,16 @@
+#include "State/Callback.hpp"
+#include "Game.hpp"
+#include "State/StatePlaying.hpp"
+
+namespace Callback {
+
+void newClicked(Game &game) {
+    game.clearStates();
+    game.pushState(std::make_unique<StatePlaying>(game));
+}
+
+void exitClicked(Game &game) { game.setRunning(false); }
+
+void continueClicked(Game &game) { game.popState(); }
+
+} // namespace Callback
