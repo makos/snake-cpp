f153629d73a73777411594e3157346f2eb6f55a4
Mateusz Makowski <matmakos@gmail.com>
2018-08-26 21:59:44 +0200

    Add versioning

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
  * This should be detached from the actual logic as much as possible.
  */
 #include "Game.hpp"
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
 
 int main(int argc, char *argv[]) {
     Game game;
