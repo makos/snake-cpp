59767353ef796af26b56bca2c594119c9afd8228
Mateusz Makowski <matmakos@gmail.com>
2018-09-01 11:35:20 +0200

    New Events

--- include/Event/Event.hpp
+++ include/Event/Event.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
-enum class Event { ClickSettings, ClickNew, ClickContinue, ClickExit };
+enum class Event {
+    ClickSettings,
+    ClickNew,
+    ClickContinue,
+    ClickExit,
+    Exit,
+    PopState,
+    StartNewGame,
+    AddScore
+};
