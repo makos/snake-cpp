f3408ff5b6294593ce11568e1021b2f5e4d6a1cd
Mateusz Makowski <matmakos@gmail.com>
2018-09-01 12:34:53 +0200

    Add Window interface

---
+++ include/Renderer/IWindow.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include "Renderer.hpp"
+#include "Util/Point.hpp"
+
+class IWindow {
+   public:
+    virtual ~IWindow() = default;
+
+    virtual void print(int y, int x, std::string msg) = 0;
+    virtual void print(int y, int x, std::string msg, Color color) = 0;
+    virtual void print(int y, int x, std::string msg, chtype attr) = 0;
+    virtual void print(Point pos, std::string msg) = 0;
+    virtual void print(Point pos, std::string msg, Color color) = 0;
+    virtual void print(Point pos, std::string msg, chtype attr) = 0;
+
+    virtual void setBox(bool state) = 0;
+    virtual int setDelay(bool state) = 0;
+    virtual int setKeypad(bool state) = 0;
+    virtual int refresh() = 0;
+    virtual int erase() = 0;
+    virtual int clear() = 0;
+    virtual int getKey() = 0;
+
+    virtual Point size() = 0;
+
+    virtual WINDOW* get() = 0;
+};
